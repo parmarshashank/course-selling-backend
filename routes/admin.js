0005004629658c942b8da1e537efd20fcfddf4a1
parmarshashank <parmarshashank6@gmail.com>
2024-11-25 14:00:25 +0530

    completed admin route

--- routes/admin.js
+++ routes/admin.js
@@ -103,7 +103,36 @@ adminRouter.post("/course", async function(req, res){
     })
 });
 
-adminRouter.put("/course", function(req, res){
+adminRouter.put("/course", async function(req, res){
+    const adminId= req.userId;
+    const {title, description, imageUrl, price, courseId}= req.body;
 
+    const course= await courseModel.updateOne({
+        _id: courseId,
+        creatorId: adminId,
+    },
+    {
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price
+
+    });
+
+    res.json({
+        message: "Course updated",
+        courseId: course._id,
+    })
 });
+
+adminRouter.get("/course/bulk", async (req, res)=>{
+    const adminId= req.userId;
+
+    const course= await courseModel.find({
+        creatorId: adminId,
+    });
+    res.json({
+        course
+    })
+})
 module.exports= {adminRouter,};
